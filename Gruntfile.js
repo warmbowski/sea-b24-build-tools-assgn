bb22ce9f781f47548444ef0a29cf6659234c6daf
Paul Laskowski <pelaskowski@gmail.com>
2014-11-30 14:33:52 -0800

    first commit: base structure and config files

---
+++ Gruntfile.js
@@ -0,0 +1,66 @@
+'use strict';
+
+module.exports = function(grunt) {
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-simple-mocha');
+  grunt.loadNpmTasks('grunt-jscs');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-browserify');
+
+  grunt.initConfig({
+    jshint: {
+      all: ['lib/**/*.js', 'client/**/*.js', 'test/**/*.js'],
+      options: {
+        jshintrc: true
+      }
+    },
+
+    jscs: {
+      src: ['lib/**/*.js', 'client/**/*.js', 'test/**/*.js'],
+      options: {
+        config: '.jscsrc'
+      }
+    },
+
+    simplemocha: {
+      src: ['test/**/*.js']
+    },
+
+    clean: {
+      dev: {
+        src: ['build/']
+      }
+    },
+
+    copy: {
+      dev: {
+        cwd: 'app/',
+        src: ['**/*.html', 'css/**/*.css' ],
+        expand: true,
+        dest: 'build/'
+      }
+    },
+
+    browserify: {
+      dev: {
+        src: ['app/js/**/*.js'],
+        dest: 'build/bundle.js',
+        options: {
+          transform: ['debowerify']
+        }
+      }
+    }
+      test: {
+        src: ['test/client/**/*test.js'],
+        dest:'test/test_bundle.js',
+        options: {
+          transform: ['debowerify']
+        }
+      }
+  });
+
+  grunt.registerTask('build:dev', ['clean:dev', 'browserify:dev', 'copy:dev']);
+  grunt.registerTask('test', ['jshint', 'jscs', 'simplemocha']);
+  grunt.registerTask('default', ['build:dev', 'test']);
+};
